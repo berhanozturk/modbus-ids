353ca289684b6a61dfe67564eb6e311a3c731268
Berhan Öztürk <109435931+berhanozturk@users.noreply.github.com>
2025-06-19 20:16:21 +0300

    Create pymodbus_server.py

---
+++ pymodbus_server.py
@@ -0,0 +1,15 @@
+from pymodbus.server import StartTcpServer
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+from pymodbus.datastore import ModbusSequentialDataBlock
+
+store = ModbusSlaveContext(
+    di=ModbusSequentialDataBlock(0, [0]*100),
+    co=ModbusSequentialDataBlock(0, [0]*100),
+    hr=ModbusSequentialDataBlock(0, [0]*100),
+    ir=ModbusSequentialDataBlock(0, [0]*100)
+)
+
+context = ModbusServerContext(slaves=store, single=True)
+
+print("✅ Modbus TCP server başlatılıyor...")
+StartTcpServer(context, address=("0.0.0.0", 1502))
